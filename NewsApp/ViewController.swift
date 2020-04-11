5514aacf3f4f94b6bf625662e12d4ca8c4ba7b37
sctpan <sctpan@qq.com>
2020-04-11 15:08:13 -0700

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by 潘一帆 on 2020/4/11.
+//  Copyright © 2020 Yifan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
